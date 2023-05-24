06e8d81765517a787be1e9ec84ff8e3c7c424d51
Aleksei Elokhin <aleksei.e@clips4sale.com>
2023-05-24 07:15:00 +0300

    page 20

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,7 @@
 package org.learn.scala
 
+import scala.annotation.unused
+
 object Main {
   def main(args: Array[String]): Unit = {
     println("Hello world!")
@@ -13,6 +15,46 @@ object Main {
 
     val a: String = "Some string value"
     println(a)
+
+    val atSymbol: Char = '@'
+    println(atSymbol)
+
+    @unused
+    val y = .15
+
+    val yellowRgb = 0xffff00
+
+    println(yellowRgb.getClass, yellowRgb)
+
+    val signature = "With Regards, \nYour friend"
+    print(signature)
+
+    val greeting = "\nHello, " + "World"
+    println(greeting)
+
+    val matched = greeting == "\nHello, World"
+    println(matched)
+
+    val MultipleLinesGreeting =
+      """She suggested reformatting the file
+        | by replacing tabs (\t) with newlines (\n);
+        | "Why do that?", he asked. """.stripMargin
+    println(MultipleLinesGreeting)
+
+    val approx = 355/113f
+    println("Pi using 355/113, is about " + approx + ".")
+    println(f"Pi using 355/113, is about $approx" + ".")
+
+    val item = "apple"
+
+    println(s"Ho do you like them ${item}s?")
+    println(s"Fish n clips n vinegar, ${"pepper "*3}salt")
+
+    println(f"I wrote a new $item%.3s today")
+    println(f"Enjoying this $item ${355/113.0}%.2f times today")
+
+
+
 
 
   }
