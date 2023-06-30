86b28ea06be1592c637be0307852de3953ed48cc
Aleksei Elokhin <aleksei.e@clips4sale.com>
2023-06-30 07:14:50 +0300

    page 102 Data structures Lists,Sets,Maps

--- src/main/scala/ListsSetsMaps.scala
+++ src/main/scala/ListsSetsMaps.scala
@@ -1,5 +1,8 @@
 package org.learn.scala
 
+import java.util
+import scala.jdk.CollectionConverters._
+
 object ListsSetsMaps {
   def main(args: Array[String]): Unit = {
 
@@ -21,20 +24,22 @@ object ListsSetsMaps {
     }
     println("Сумма всех i =" + " " + total)
 
-//    for (c <- colors) { println(c) }
+    //    for (c <- colors) { println(c) }
 
-    colors.foreach( (c: String) => println(c))
+    colors.foreach((c: String) => println(c))
 
-    val sizes = colors.map( (c: String) => c.size)
+    val sizes = colors.map((c: String) => c.size)
     println(sizes)
 
-    val tot = numbers.reduce( (a: Int, b: Int) => { println(a, b); a + b } )
+    val tot = numbers.reduce((a: Int, b: Int) => {
+      println(a, b); a + b
+    })
     println(tot)
 
     val unique = Set(10, 20, 30, 20, 20, 10)
     println(unique)
 
-    val sum = unique.reduce( (a: Int, b: Int) =>  a + b )
+    val sum = unique.reduce((a: Int, b: Int) => a + b)
     println(sum)
 
     val colorMap = Map("red" -> 0xFF0000, "green" -> 0xFF00,
@@ -49,7 +54,9 @@ object ListsSetsMaps {
     val hasWhite = colorMap.contains("white")
     println(hasWhite)
 
-    for (pairs <- colorMap) { println(pairs) }
+    for (pairs <- colorMap) {
+      println(pairs)
+    }
 
     val oddsAndEvents = List(List(1, 3, 5), List(2, 4, 6))
     println(oddsAndEvents.flatten)
@@ -59,7 +66,7 @@ object ListsSetsMaps {
 
     val primes = List(2, 3, 4, 5, 7, 11, 13)
 
-//    val first = primes(0)
+    //    val first = primes(0)
     val first = primes.head
     val four = primes(3)
     val remaining = primes.tail
@@ -68,15 +75,24 @@ object ListsSetsMaps {
 
     var i = primes
 
-    while (! i.isEmpty) { print(i.head + ", "); i = i.tail }
+    while (!i.isEmpty) {
+      print(i.head + ", "); i = i.tail
+    }
     print("\n")
 
-    def visit(i: List[Int]) { if (i.size > 0) { print(i.head + ", ");
-      visit(i.tail) } }
+    def visit(i: List[Int]) {
+      if (i.size > 0) {
+        print(i.head + ", ");
+        visit(i.tail)
+      }
+    }
+
     println("visit output:")
     visit(primes)
 
-    while (i != Nil) { print(i.head + ", "); i = i.tail }
+    while (i != Nil) {
+      print(i.head + ", "); i = i.tail
+    }
     print("\n")
 
     val l: List[Int] = List()
@@ -119,7 +135,7 @@ object ListsSetsMaps {
     println(p)
     println(s)
 
-// List appending operations
+    // List appending operations
     val appended = List(1, 2, 3, 4) :+ 5
     val suffix = appended takeRight (3)
     val middle = suffix dropRight 2
@@ -127,17 +143,17 @@ object ListsSetsMaps {
     println(suffix)
     println(middle)
 
-//    Mapping Lists
+    //    Mapping Lists
 
     println(List(0, 1, 0) collect {
       case 1 => "OK"
       case 0 => "Error"
     })
 
-    println(List("milk,tea") flatMap(_.split(',')))
-    println(List("milk","tea") map(_.toUpperCase))
+    println(List("milk,tea") flatMap (_.split(',')))
+    println(List("milk", "tea") map (_.toUpperCase))
 
-//    Math reduction operations
+    //    Math reduction operations
     println(List(41, 59, 26).max)
     println(List(10.9, 32.5, 4.23, 5.67).min)
     println(List(5, 6, 7).product)
@@ -151,17 +167,21 @@ object ListsSetsMaps {
     val validations = List(true, true, false, true, true, true)
     val valid1 = !(validations contains false)
     println(valid1)
-    val valid2 = validations forall(_ == true)
+    val valid2 = validations forall (_ == true)
     println(valid2)
     val valid3 = validations.exists(_ == false) == false
     println(valid3)
 
     println("=" * 50)
+
     def contains(x: Int, l: List[Int]): Boolean = {
       var a: Boolean = false
-      for (i <- l) { if (!a) a = (i == x)}
+      for (i <- l) {
+        if (!a) a = (i == x)
+      }
       a
     }
+
     val included = contains(19, List(46, 19, 92))
     println(included)
 
@@ -172,7 +192,7 @@ object ListsSetsMaps {
       a
     }
 
-    val  included2 = boolReduce(List(46, 19, 92), false) { (a, i) =>
+    val included2 = boolReduce(List(46, 19, 92), false) { (a, i) =>
       if (a) a else i == 19
     }
     println(included2)
@@ -182,6 +202,7 @@ object ListsSetsMaps {
       for (i <- l) a = f(a, i)
       a
     }
+
     val included3 = reduceOp(List(46, 19, 92), false) { (a, i) =>
       if (a) a else i == 19
     }
@@ -218,28 +239,50 @@ object ListsSetsMaps {
     println(List(2, 5, 5, 3, 2).toSet)
     println(List(2, 5, 5, 3, 2).toString())
 
+    println("#" * 50)
 
+    println(List(12, 29).asJava)
+    new util.ArrayList[5]().asScala
+    val statuses = List(500, 404)
+    val msg = statuses.head match {
+      case x if x < 500 => "OK"
+      case _ => "whoah, an error"
+    }
 
+    println(msg)
+    val msg2 = statuses match {
+      case x if x contains (500) => "has error"
+      case _ => "okay"
+    }
+    println(msg2)
 
+    val msg3 = statuses match {
+      case List(404, 500) => "not found & error"
+      case List(500, 404) => "error & not found"
+      case List(200, 200) => "okay"
+      case _ => "not sure what happened"
+    }
+    println(msg3)
 
+    val msg4 = statuses match {
+      case List(500, x) => s"Error followed by $x"
+      case List(e, x) => s"$e followed by $x"
+    }
+    println(msg4)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    val head = List('r', 'g', 'b') match {
+      case x :: xs => x
+      case Nil => ' '
+    }
+    println(head)
+    val code = ('h', 204, true) match {
+      case (_, _, false) => 501
+      case ('c', _, true) => 302
+      case ('h', x, true) => x
+      case (c, x, true) => {
+        println(s"Did not expect code $c")
+      }
+    }
+    println(code)
   }
 }
